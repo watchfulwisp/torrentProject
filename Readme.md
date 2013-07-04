7373166a1e0e4eff3f23eb15446dc2c1735519ab
Alexandru Marian Vasile <alexandru.vasile@cmbtravel.ro>
2013-07-04 17:04:54 +0300

    first commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# torrentProject
+
+A node.js library for accessing the Torrent Project REST API.
---
+++ demo.js
@@ -0,0 +1,21 @@
+var tp = require('./lib/torrentProject.js');
+
+tp.search('debian 7 dvd', 'matches', 'json', function(err, data){
+	console.log(err, data);
+});
+
+/*tp.trackers('8ac3731ad4b039c05393b5404afa6e7397810b41', 'json', function(err, data){
+	console.log(err, data);
+});
+
+tp.peers('8ac3731ad4b039c05393b5404afa6e7397810b41', function(err, data){
+	console.log(err, data);
+});
+
+tp.getHourlyDump('hourlydump', function(err, data){
+	console.log(err, data);
+});
+
+tp.getDailyDump('dailydump', function(err, data){
+	console.log(err, data);
+});*/
